b5cdeac18f01de2c258beffa157877cf0d691d25
aleksey <a.baranov89@mail.ru>
2024-01-29 18:23:08 +0500

    Initial Commit

---
+++ RB/RBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RBApp.swift
+//  RB
+//
+//  Created by Алексей Баранов on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
